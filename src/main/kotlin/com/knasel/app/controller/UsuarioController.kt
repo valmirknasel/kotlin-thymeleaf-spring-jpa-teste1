109ac04dbb129e425baa0ac42f5ef7696cd9ff38
Valmir Ademar Weide Knasel Junior <valmirknasel@gmail.com>
2020-10-28 20:54:31 -0400

    Corrigida a Anotação @Controller para @RestController, o que estava causando falha para exibir as respostas em formato JSON no browser

--- src/main/kotlin/com/knasel/app/controller/UsuarioController.kt
+++ src/main/kotlin/com/knasel/app/controller/UsuarioController.kt
@@ -4,18 +4,20 @@ import com.knasel.app.model.Usuario
 import com.knasel.app.repository.UsuarioRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/usuarios")
 class UsuarioController(val repository: UsuarioRepository) {
     var logger:Logger = LoggerFactory.getLogger(javaClass)
 
     @RequestMapping("/{nome}")
     fun findUsuarioByNome(@PathVariable nome: String): MutableList<Usuario> {
-        return repository.findUsuarioByNome(nome).also { logger.info("") }
+        var usr = repository.findUsuarioByNome(nome)
+        logger.info(usr.toString())
+        return usr
     }
 
 }
