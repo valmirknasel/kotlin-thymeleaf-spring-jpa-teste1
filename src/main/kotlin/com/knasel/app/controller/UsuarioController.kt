335d1f73289f6d4cb69e9ebd56b1ebaaab080e17
Valmir Ademar Weide Knasel Junior <valmirknasel@gmail.com>
2020-10-28 23:36:24 -0400

    Acrescentados alguns mapeamentos de rotas para usuario

--- src/main/kotlin/com/knasel/app/controller/UsuarioController.kt
+++ src/main/kotlin/com/knasel/app/controller/UsuarioController.kt
@@ -7,15 +7,30 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/usuarios")
 class UsuarioController(val repository: UsuarioRepository) {
     var logger:Logger = LoggerFactory.getLogger(javaClass)
 
-    @RequestMapping("/{nome}")
+    @RequestMapping("/nome/{nome}")
     fun findUsuarioByNome(@PathVariable nome: String): MutableList<Usuario> {
-        var usr = repository.findUsuarioByNome(nome)
+        val usr = repository.findUsuarioByNome(nome)
+        logger.info(usr.toString())
+        return usr
+    }
+
+    @RequestMapping("/listar")
+    fun findAll(): MutableList<Usuario> {
+        val usrList = repository.findAll()
+        logger.info(usrList.toString())
+        return usrList
+    }
+
+    @RequestMapping("/{idUsuario}")
+    fun findUsuario(@PathVariable idUsuario: Long): Optional<Usuario> {
+        val usr = repository.findById(idUsuario)
         logger.info(usr.toString())
         return usr
     }
