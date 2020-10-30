d0de831fdbf65538d852d2e450619af61b971a42
Valmir Ademar Weide Knasel Junior <valmirknasel@gmail.com>
2020-10-30 00:52:07 -0400

    Adicionada funcionalidade de log no HomeController

--- src/main/kotlin/com/knasel/app/controller/HomeController.kt
+++ src/main/kotlin/com/knasel/app/controller/HomeController.kt
@@ -1,12 +1,19 @@
 package com.knasel.app.controller
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
 
 @RestController
 @RequestMapping("/")
 class HomeController {
+    val logger:Logger = LoggerFactory.getLogger(javaClass)
 
-    fun home() = "index"
-
+    fun home():String {
+      logger.info("at home!")
+        return "index"
+    }
 }
