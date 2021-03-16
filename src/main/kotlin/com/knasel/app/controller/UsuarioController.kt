b1eb5592b5da5c904f06d1fbc5c901eec3cbcd78
Valmir Ademar Weide Knasel Junior <valmirknasel@gmail.com>
2021-03-16 03:03:31 -0400

    Corrigidos os mapeamentos do método findAll() e findUsuario by Id.

--- src/main/kotlin/com/knasel/app/controller/UsuarioController.kt
+++ src/main/kotlin/com/knasel/app/controller/UsuarioController.kt
@@ -1,13 +1,10 @@
 package com.knasel.app.controller
 
-import com.knasel.app.model.Usuario
 import com.knasel.app.repository.UsuarioRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 
 @RestController
@@ -15,30 +12,28 @@ import org.springframework.web.servlet.ModelAndView
 class UsuarioController(val repository: UsuarioRepository) {
     var logger:Logger = LoggerFactory.getLogger(javaClass)
 
-    @RequestMapping("/nome/{nome}")
+    /* Falta testar se ta funcionando corretamente */
+    @GetMapping("/nome/{nome}")
     fun findUsuarioByNome(@PathVariable nome: String): ModelAndView {
         val usr = repository.findUsuarioByNome(nome)
-        logger.info("findUsuarioByNome: $usr")
+        logger.info("findUsuarioByNome: $usr") // loga os dados do usuário trazido do banco
         return ModelAndView("usuario").addObject("usr",usr)
     }
 
-    @RequestMapping("/listar")
-    fun findAll(): ModelAndView {// pra retornar usrList tem q alterar o tipo de retorno do método pra MutableList<Usuario>
-        val usrList = repository.findAll()
-        logger.info(usrList.toString())
-        /* Se usar o return usrList ao invés de retornar um ModelAndView e alterar o tipo de retorno de ModelAndView
-        pra MutableList<Usuario> o resultado é um JSON. Ficaria assim:
-        return usrList
-        */
-        return ModelAndView("usuario").addObject("usrList",usrList)
+    @GetMapping("/listar")
+    fun findAll(): ModelAndView {
+        val usrList = repository.findAll() //substituir por um repositorioService para desacoplar o código
+
+        logger.info("findAll: $usrList") // loga os dados da lista
+        return ModelAndView("usuarioLista").addObject("listaDeUsuarios",usrList)
 
     }
 
     /*Tá funcionando e exibindo na view!!! 15/03/2021*/
-    @RequestMapping("/{idUsuario}")
-    fun findUsuario(@PathVariable idUsuario: Long, model:Model): ModelAndView {
+    @GetMapping("/{idUsuario}")
+    fun findUsuario(@PathVariable idUsuario: Long): ModelAndView {
         val usr = repository.findById(idUsuario).get()
-        logger.info("findUsuario: $usr")
+        logger.info("findUsuario: $usr") // loga os dados do usuário trazido do banco
         return ModelAndView("usuario").addObject("usr",usr)
     }
 
